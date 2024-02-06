b11746658db81441afeeac4f108a217aa78039ae
Alan Werner Hassan <alan.hassan@shopee.com>
2024-02-05 21:02:19 -0300

    atualizando

--- stocks_project/database.py
+++ stocks_project/database.py
@@ -5,15 +5,14 @@ from github import Github
 import os
 
 
-
 # updated database with recent matches from github
-
 def get_df():
     url_df = 'https://github.com/alanhassan/stocks_panel/blob/main/df.csv?raw=true'
     data = requests.get(url_df).content
     df = pd.read_csv(BytesIO(data))
     df = df.sort_values('Empresa')
     df = df[df['Setor'].notnull()]
+    df['Patrim. Líq'] = df['Patrim. Líq'].apply(lambda x: f"{x:,.0f}")
     return df
 
 df = get_df()
--- stocks_project/routes.py
+++ stocks_project/routes.py
@@ -3,9 +3,9 @@ from stocks_project import app, database
 from stocks_project.database import df
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/stocks')
 def stocks():
     return render_template('stocks.html', dataframe=df)
+
+# @app.route('/stocks')
+# def stocks():
+#     return render_template('stocks.html', dataframe=df)
